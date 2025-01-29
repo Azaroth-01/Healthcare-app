36da8c383e749a73d595a542874f3ecd2b090d58
Aarya <aaryamaan.106@gmail.com>
2025-01-29 18:55:09 +0530

    fifth commit

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -10,11 +10,11 @@ const Registration=async({params: {userId}}:SearchParamProps)=>{
 
     return(
         
-            <div className="flex h-screen max-hhscreen">
+            <div className="flex h-screen max-h-screen">
               
               <section className="remove-scroller container my-auto">
         
-                <div className="sub-container max-w-[496px]">
+                <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
                   <Image
                   src="/assets/icons/logo-full.svg"
                   height={1000}
@@ -26,12 +26,9 @@ const Registration=async({params: {userId}}:SearchParamProps)=>{
         
                   <RegistrationForm user={user} />
         
-                  <div className="text-14-regular mt-20 flex justify-between">
-                 <p className="justify-items-end text-dark-600 xl;text-left">©2025 CarePulse
-                  </p> 
-                  <Link href="/?admin=true" className="text-green-500">Admin
-                  </Link>
-                  </div>
+                  <p className="copyright py-12 ">©2025 CarePulse
+                  </p>
+                  
                 </div>
               </section>
         
